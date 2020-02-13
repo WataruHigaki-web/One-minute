b73b5c7a1a8c8a2ed73b130c2e794f079e4725ce
WataruHigaki-web <WataruHigaki-web>
2020-02-13 02:59:31 +0000

    Finish user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,3 @@
 class ApplicationController < ActionController::Base
-    def index
-  end
-  def new
-  	@daybook = Daybook.new(daybook_params)
-  end
-
-
-  def create 
-  	@daybook = Daybook.new(daybook_params)
-  	if daybook .save
-  		redirect_to 
-  	else
-  		render 'new'
-  	end
-  end
-
-   private
-
-  def daybook_params
-  	params.require(:daybook).permit(:title, :body, :user_id)
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
   resources :daybooks
   root 'daybooks#home'
   get '/help' => 'daybooks#help'
   get '/signup' => 'users#new'
+  post '/signup',to: 'users#create'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
